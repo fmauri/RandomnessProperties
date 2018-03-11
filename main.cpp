57f41f5ce3727ceea5b290564446c7b9701a1e3d
fmauri <mauri95francesco@gmail.com>
2018-03-11 20:47:52 +0100

    reading file

--- main.cpp
+++ main.cpp
@@ -1,6 +1,26 @@
+#include <string>
+#include <fstream>
 #include <iostream>
+#include <vector>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::string letter;
+    int i;
+    std::string line;
+    std::vector<std::string> result;
+    std::ifstream reader("../lfsrs/output.txt");
+    if (!reader) {
+        std::cout << "Error opening input file" << std::endl;
+        return -1;
+    }
+
+    for (i = 0; !reader.eof(); i++) {
+        getline(reader, line);
+        result.push_back(line);
+    }
+    for (auto const &item : result) {
+        std::cout << item << std::endl;
+    }
+    
     return 0;
 }
