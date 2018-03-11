2d22593e088e31d4d08c04578fdb0fe6e1051e18
fmauri <mauri95francesco@gmail.com>
2018-03-11 20:18:04 +0100

    firster commit

---
+++ cmake-build-debug/CMakeFiles/RandomnessProperties.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RandomnessProperties.dir/main.cpp.o"
+  "RandomnessProperties.pdb"
+  "RandomnessProperties"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/RandomnessProperties.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
